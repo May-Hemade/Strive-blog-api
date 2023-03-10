b9459a959329018b944a4849618bf01e24770ea4
May Hemade <71593691+May-Hemade@users.noreply.github.com>
2023-03-10 11:49:32 +0200

    main

--- src/lib/fs-tools.js
+++ src/lib/fs-tools.js
@@ -6,24 +6,24 @@ const { readJSON, writeJSON, writeFile, createReadStream, createWriteStream } = 
 
 const apiFolderPath = join(dirname(fileURLToPath(import.meta.url)), "../api")
 const publicFolderPathAuthors = join(process.cwd(), "./public/img/authors")
-const publicFolderPathBlogs = join(process.cwd(), "./public/img/blogPosts")
+export const publicFolderPathBlogs = join(process.cwd(), "./public/img/blogPosts")
 
 console.log("ROOT OF THE PROJECT:", process.cwd())
 // console.log("PUBLIC FOLDER:", publicFolderPath)
 
 console.log("DATA FOLDER PATH: ", apiFolderPath)
 const authorsJSONPath = join(apiFolderPath, "/authors/authors.json")
-const blogsJSONPath = join(apiFolderPath, "/Blogs/blogs.json")
+const blogsJSONPath = join(apiFolderPath, "/blogs/blogs.json")
+const dataFolderPath = join(dirname(fileURLToPath(import.meta.url)), "../data")
 
 export const getAuthors = () => readJSON(authorsJSONPath)
 export const writeAuthors = (authorsArray) => writeJSON(authorsJSONPath, authorsArray)
 export const getBlogs = () => readJSON(blogsJSONPath)
 export const writeBlogs = (blogsArray) => writeJSON(blogsJSONPath, blogsArray)
 
-export const saveAuthorsAvatars = (fileName, contentAsABuffer) => writeFile(join(publicFolderPathAuthors, fileName), contentAsABuffer) //use this in tools
+export const saveAuthorsAvatars = (fileName, contentAsABuffer) => writeFile(join(publicFolderPathAuthors, fileName), contentAsABuffer)
 
 export const saveBlogsCover = (fileName, contentAsABuffer) => writeFile(join(publicFolderPathBlogs, fileName), contentAsABuffer)
 
-export const getBlogJsonReadableStream = () => createReadStream(blogsJSONPath)
-// export const getPDFWritableStream = (filename) =>
-//   createWriteStream(join(dataFolderPath, filename))
+export const getBlogJsonReadableStream = () => createReadStream(blogsJSONPath) //to read from csv comma seperated  values
+export const getPDFWritableStream = (filename) => createWriteStream(join(publicFolderPathBlogs, filename)) // gives me a way to save a file
